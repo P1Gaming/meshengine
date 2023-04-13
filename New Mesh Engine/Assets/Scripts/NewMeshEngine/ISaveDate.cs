f3cdc5afa3971b6ef850515b75e580329dc3179c
Dragyn85 <nygard.jonas@gmail.com>
2023-04-13 22:07:40 +0200

    moved ChunkData and BlockType to its own file

--- New Mesh Engine/Assets/Scripts/NewMeshEngine/ISaveDate.cs
+++ New Mesh Engine/Assets/Scripts/NewMeshEngine/ISaveDate.cs
@@ -1,16 +1,4 @@
-using UnityEngine;
-
 public interface ISaveDate
 {
     public void SaveChunkData (ChunkData chunkData);
-}
-public class ChunkData
-{
-    bool isEmpty;
-    BlockType[][][] Data;
-    Vector3Int position;
-}
-public enum BlockType
-{
-
 }
