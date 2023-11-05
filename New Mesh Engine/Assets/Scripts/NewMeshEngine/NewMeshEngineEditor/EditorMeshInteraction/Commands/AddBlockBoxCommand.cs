9df330ade30f78b5756adeb2b58c6ebff98901a5
Dragyn85 <nygard.jonas@gmail.com>
2023-11-05 22:56:39 +0100

    Changed box add tool to use IMeshEngine AddBlocks instead of doing multiple TryAddBlock

--- New Mesh Engine/Assets/Scripts/NewMeshEngine/NewMeshEngineEditor/EditorMeshInteraction/Commands/AddBlockBoxCommand.cs
+++ New Mesh Engine/Assets/Scripts/NewMeshEngine/NewMeshEngineEditor/EditorMeshInteraction/Commands/AddBlockBoxCommand.cs
@@ -8,7 +8,7 @@ public class AddBlockBoxCommand : ICommand
     //Vector3 maxPos;
     BlockType blockType;
     BlockType[,,] previousBlockTypes;
-    private List<BlockTypeWithPosition> previousBlockTypesWithPosition;
+    private List<BlockTypeWithPosition> previousBlockTypesWithPosition = new List<BlockTypeWithPosition>();
     BoundsInt bounds;
 
     public AddBlockBoxCommand(Vector3 minPos, Vector3 maxPos, BlockType blockType)
@@ -18,6 +18,7 @@ public class AddBlockBoxCommand : ICommand
         bounds.max = Vector3Int.RoundToInt(maxPos);
         this.blockType = blockType;
     }
+
     public string GetCommandName()
     {
         return "Block box";
@@ -29,27 +30,12 @@ public class AddBlockBoxCommand : ICommand
         {
             return false;
         }
-        
+
         previousBlockTypes = new BlockType[bounds.size.x, bounds.size.y, bounds.size.z];
         Vector3Int minPos = Vector3Int.RoundToInt(bounds.min);
-        for (int x = 0; x < bounds.size.x; x++)
-        {
-            for (int y = 0; y < bounds.size.y; y++)
-            {
-                for (int z = 0; z < bounds.size.z; z++)
-                {
-                    Vector3Int position = new Vector3Int(x, y, z)+minPos;
-                    previousBlockTypes[x, y, z] = ResourceReferenceKeeper.GetResource<IMeshEngine>().GetBlockType(position);
-                    ResourceReferenceKeeper.GetResource<IMeshEngine>().TryAddBlock(Vector3Int.RoundToInt(position), blockType);
-                }
-            }
-        }
-        return true;
-    }
-    
-    public void Undo()
-    {
-        Vector3Int minPos = Vector3Int.RoundToInt(bounds.min);
+        List<BlockTypeWithPosition> blockTypeWithPositions = new();
+
+        var me = ResourceReferenceKeeper.GetResource<IMeshEngine>();
         for (int x = 0; x < bounds.size.x; x++)
         {
             for (int y = 0; y < bounds.size.y; y++)
@@ -57,11 +43,30 @@ public class AddBlockBoxCommand : ICommand
                 for (int z = 0; z < bounds.size.z; z++)
                 {
                     Vector3Int position = new Vector3Int(x, y, z) + minPos;
-                    var me = ResourceReferenceKeeper.GetResource<IMeshEngine>();
-                    me.TryRemoveBlock(position);
-                    me.TryAddBlock(Vector3Int.RoundToInt(position), previousBlockTypes[x,y,z]);
+                    previousBlockTypesWithPosition.Add(new BlockTypeWithPosition(me.GetBlockType(position), position));
+                    //previousBlockTypes[x, y, z] = me.GetBlockType(position);
+                    blockTypeWithPositions.Add(new BlockTypeWithPosition(blockType, position));
+                    //ResourceReferenceKeeper.GetResource<IMeshEngine>().TryAddBlock(Vector3Int.RoundToInt(position), blockType);
                 }
             }
         }
+
+        me.AddBlocks(blockTypeWithPositions);
+        return true;
     }
+
+    public void Undo()
+    {
+        Vector3Int minPos = Vector3Int.RoundToInt(bounds.min);
+        var me = ResourceReferenceKeeper.GetResource<IMeshEngine>();
+        List<Vector3Int> blockPositionsToRemove = new();
+        foreach (var BlockType in previousBlockTypesWithPosition)
+        {
+            blockPositionsToRemove.Add(BlockType.Position);
+        }
+
+        me.TryRemoveBlocks(blockPositionsToRemove,out List<BlockTypeWithPosition> blockTypeAtPosition);
+        me.AddBlocks(previousBlockTypesWithPosition);
+    }
+
 }
